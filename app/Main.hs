44a3796cd04779df7b9e3d9de64a7dab48dd37a1
Krushan Bauva <krushan-hasura@Krushans-MacBook-Pro.local>
2022-08-22 18:19:25 +0530

    add week 4 homework exercises

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Hw01
 import Hw03.Golf
+import Hw04
 
 main :: IO ()
 main = do 
@@ -10,3 +11,9 @@ main = do
     print $ skips "hello!"
     print $ localMaxima [2,9,5,6,1]
     putStr $ histogram [1,4,5,4,6,6,3,4,2,4,9]
+    print $ fun1' [3,8,9,15,12,16,19,24]
+    print $ fun2' 123
+    print $ xor [False, True, False, False, True, False, True]
+    print $ map' (*3) [1..10]
+    print $ myFoldl (-) 0 [1..5]
+    print $ sieveSundaram 100
---
+++ src/Hw04.hs
@@ -0,0 +1,33 @@
+module Hw04 (fun1', fun2', xor, map', myFoldl, sieveSundaram) where
+
+import Data.Set (Set, difference, fromList, toList)
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' xs = product $ map (subtract 2) (filter even xs)
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n  | even n = n + fun2 (n `div` 2)
+        | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' n = iterate (\x -> if even x then x + fun2 (x `div` 2) else fun2 (3 * x + 1)) n !! 1
+
+xor :: [Bool] -> Bool
+xor = foldr (/=) False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x y -> f x : y) []
+
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr (flip f) base (reverse xs)
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = [2*x+1 | x <- toList $ difference (fromList [1..n]) (fromList removeList)]
+                  where removeList = [i+j+2*i*j | i <- [1 .. n `div` 2], j <- [1 .. n `div` 2], i <= j, i+j+2*i*j <= n]
